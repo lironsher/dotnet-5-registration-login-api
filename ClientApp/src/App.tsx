87b064315ab26c6ee447aa4e3576ae5e22fe4131
Lirons <liron.sher@gmail.com>
2021-08-20 11:38:14 +0300

    add base react ts app

---
+++ ClientApp/src/App.tsx
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Index from './components/Index';
+import GuestRoute from './utils/GuestRoute';
+import PrivateRoute from './utils/PrivateRoute';
+//redux stuff
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { CheckAuthentication } from './utils/CheckAuthentication';
+const App: React.FC = () => {
+  useEffect(() => {
+    CheckAuthentication();
+  }, []);
+  return (
+    <div className="App">
+      <Provider store={store}>
+        <Router>
+          <Switch>
+            <PrivateRoute exact path="/" component={Index} />
+            <GuestRoute exact path="/login" component={Login} />
+          </Switch>
+        </Router>
+      </Provider>
+    </div>
+  );
+};
+export default App;
---
+++ ClientApp/src/components/Index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Index() {
+  return (
+    <div className="App">
+      <header className="App-header">index</header>
+    </div>
+  );
+}
+
+export default Index;
